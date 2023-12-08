1a67b70dcbbc8e9617e472d326782f02afc0cd0c
krishna <33350985+kushwaha03@users.noreply.github.com>
2023-12-08 13:08:48 +0530

    Initial Commit

---
+++ Country/CountryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryApp.swift
+//  Country
+//
+//  Created by Radhe on 08/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CountryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
